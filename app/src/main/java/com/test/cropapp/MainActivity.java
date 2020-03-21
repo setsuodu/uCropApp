9a2b0579b47bc86aa2da17cb84b1525525b76366
Xue <setsuodu@users.noreply.github.com>
2020-03-21 15:48:09 +0800

    集成android-crop
    Signed-off-by: Xue <setsuodu@users.noreply.github.com>

--- app/src/main/java/com/test/cropapp/MainActivity.java
+++ app/src/main/java/com/test/cropapp/MainActivity.java
@@ -2,18 +2,28 @@ package com.test.cropapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.soundcloud.android.crop.Crop;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+    private ImageView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultView = (ImageView) findViewById(R.id.result_image);
 
         //按钮Button
         Button mButton = (Button) findViewById(R.id.button);
@@ -27,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             if (v.getId()==R.id.button){
                 Toast.makeText(MainActivity.this, "您点击了控件："+((TextView)v).getText(), Toast.LENGTH_SHORT).show();
+
+                onPick();
             }
         }
     }
@@ -37,6 +49,29 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "您长按了控件："+((TextView)v).getText(), Toast.LENGTH_SHORT).show();
             }
             return false;
+        }
+    }
+
+    public void onPick() {
+        Crop.pickImage(this);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == Crop.REQUEST_PICK && resultCode == RESULT_OK) {
+            beginCrop(result.getData());
+        } else if (requestCode == Crop.REQUEST_CROP) {
+            handleCrop(resultCode, result);
+        }
+    }
+    private void beginCrop(Uri source) {
+        Uri destination = Uri.fromFile(new File(getCacheDir(), "cropped"));
+        Crop.of(source, destination).asSquare().start(this);
+    }
+    private void handleCrop(int resultCode, Intent result) {
+        if (resultCode == RESULT_OK) {
+            resultView.setImageURI(Crop.getOutput(result));
+        } else if (resultCode == Crop.RESULT_ERROR) {
+            Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 }
